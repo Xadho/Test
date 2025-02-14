72aeeca8f3216a0970253b0c45ae57b165839b5c
mdejong <mj22@curio.nl>
2025-02-14 14:06:43 +0100

    test

--- main.py
+++ main.py
@@ -3,13 +3,13 @@ import time
 import os
 
 #Read check values
-file1 = open("rules/age.txt", "r")
-age_check = int(file1.read())
-file1.close()
+file_ag = open("rules/age.txt", "r")
+age_check = int(file_ag.read())
+file_ag.close()
 
 file2 = open("rules/height.txt", "r")
 height_check = int(file2.read())
-file1.close()
+file_ag.close()
 
 running = True
 while running:
